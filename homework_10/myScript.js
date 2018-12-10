805775406d569de2e365cbfe438daac614085fc5
oksanagaranzha <44702479+oksanagaranzha@users.noreply.github.com>
2018-12-10 22:39:59 +0300

    changes

---
+++ homework_10/myScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+document.getElementById("demo").style.color="red";
+}
+function myFunction1() {
+document.getElementById("demo1").style.width="200px";
+}
+function myFunction2() {
+document.getElementById("demo2").style.border="5px solid red";
+}
